91273a9586e17ce2ac2f843c58882aa270cd368a
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-11-26 22:56:53 +0530

    next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript:{
-        ignoreBuildErrors:true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "*",
-            }
-        ]
-    },
-    eslint:{
-        ignoreDuringBuilds:true,
-    },
-    devIndicators: {
-        appIsrStatus: true,
-        buildActivity: true,
-        buildActivityPosition: "bottom-right",
-    },
-    experimental: {
-        after: true,
-        ppr: 'incremental',
-    }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
+  experimental: {
+    after: true,
+    ppr: "incremental",
+  },
 };
 
 export default nextConfig;
