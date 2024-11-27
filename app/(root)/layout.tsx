a0db5f77ee6492a33ea2d9e54345ad99d613160d
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-11-27 13:43:59 +0530

    added a footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,6 +9,7 @@ export default function Layout({
     <main className="font-work-sans">
       <Navbar />
       {children}
+      <h1 className="text-center p-2">Made by Govind❤️</h1>
     </main>
   );
 }
